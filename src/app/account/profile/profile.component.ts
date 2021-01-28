1727608b0da1d1bab151d7916110b029bf411731
Seppe Alaerts <r0749143@student.thomasmore.be>
2021-01-28 09:53:23 +0100

    value data + edit data

--- src/app/account/profile/profile.component.ts
+++ src/app/account/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { DatePipe, formatDate } from '@angular/common';
 import { User } from 'src/app/shared/models/user.model';
 import { AccountService } from '../services/account.service';
 
@@ -11,15 +12,30 @@ import { AccountService } from '../services/account.service';
 export class ProfileComponent implements OnInit {
 
   user!: User;
+  created_at!: string;
 
-  constructor(private titleService: Title, public accountService: AccountService,) {
+  disabled: boolean = true;
+  loading: boolean = false;
+
+  constructor(private titleService: Title, public accountService: AccountService) {
     this.titleService.setTitle("Profiel - Smart City Herentals");
     this.accountService.user.subscribe(result => {
       this.user = result;
+      const format = 'dd/MM/yyyy';
+      const locale = 'en-US';
+      this.created_at = formatDate(this.user.created_at!, format, locale);
     });
   }
 
   ngOnInit(): void {
+  }
+
+  editProfile() {
+    this.disabled = false;
+  }
+
+  updateProfile() {
+    this.loading = true;
   }
 
 }
