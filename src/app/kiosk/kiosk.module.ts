f0849fd6d38c47dd85d29bff232ad3cb5c285031
Seppe Alaerts <r0749143@student.thomasmore.be>
2021-01-31 12:04:20 +0100

    question component

--- src/app/kiosk/kiosk.module.ts
+++ src/app/kiosk/kiosk.module.ts
@@ -5,11 +5,12 @@ import { SurveyComponent } from './survey/survey.component';
 import { SurveyListComponent } from './survey-list/survey-list.component';
 import { CommunicationComponent } from './communication/communication.component';
 import { MessageDetailsComponent } from './message-details/message-details.component';
-import { MaterialModule } from '../shared/material.module'
+import { MaterialModule } from '../shared/material.module';
+import { QuestionComponent } from './question/question.component'
 
 
 @NgModule({
-  declarations: [KioskHomeComponent, SurveyComponent, SurveyListComponent, CommunicationComponent, MessageDetailsComponent],
+  declarations: [KioskHomeComponent, SurveyComponent, SurveyListComponent, CommunicationComponent, MessageDetailsComponent, QuestionComponent],
   imports: [
     CommonModule,
     MaterialModule
