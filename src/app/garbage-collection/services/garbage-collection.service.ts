608fefe442d0119aaa751a64983f557d4da7a308
Jasper Guldentops <r0760673@student.thomasmore.be>
2021-02-04 12:50:33 +0100

    Easy assignment for bins to zone added

--- src/app/garbage-collection/services/garbage-collection.service.ts
+++ src/app/garbage-collection/services/garbage-collection.service.ts
@@ -11,10 +11,19 @@ export class GarbageCollectionService {
 
   constructor(private http: HttpClient) { }
 
-  private url = "http://smartcityapi.seppealaerts.be/api";
+  //private url = "http://smartcityapi.seppealaerts.be/api";
+  private url = "http://localhost:8000/api";
 
   getBinInfoList(): Observable<BinInfo[]> {
     return this.http.get<BinInfo[]>(this.url + '/bininfo');
+  }
+
+  getBinInfoListByZone(id: number): Observable<BinInfo[]> {
+    return this.http.get<BinInfo[]>(this.url + '/bininfo/byzone/' + id);
+  }
+
+  getBinInfoListNozone(): Observable<BinInfo[]> {
+    return this.http.get<BinInfo[]>(this.url + '/bininfo/nozone');
   }
 
   getBinInfo(id: number): Observable<BinInfo> {
@@ -27,6 +36,10 @@ export class GarbageCollectionService {
 
   updateBinInfo(binInfo: BinInfo): Observable<BinInfo> {
     return this.http.put<BinInfo>(this.url + '/bininfo/' + binInfo.id, binInfo);
+  }
+
+  updateZoneBins(id: number, bin_id_list: number[]): Observable<BinInfo> {
+    return this.http.put<BinInfo>(this.url + '/zones/bins/' + id, { 'bin_id_list': bin_id_list});
   }
 
   deleteBinInfo(id: number) {
