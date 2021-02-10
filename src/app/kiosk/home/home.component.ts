485eb85e363f7ce26ef67ffd4f919b00c2979a83
Seppe Alaerts <r0749143@student.thomasmore.be>
2021-02-10 22:35:00 +0100

    message previews on home

--- src/app/kiosk/home/home.component.ts
+++ src/app/kiosk/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { Router } from '@angular/router';
+import { AlertService } from 'src/app/shared/alert/services/alert.service';
+import { Message } from 'src/app/shared/models/message.model';
 import { Survey } from 'src/app/shared/models/survey.model';
 import { KioskService } from '../services/kiosk.service';
 
@@ -11,11 +13,25 @@ import { KioskService } from '../services/kiosk.service';
 })
 export class KioskHomeComponent implements OnInit {
 
-  constructor(private titleService: Title, private kioskService: KioskService, private router: Router) {
+  messages: Message[] = [];
+
+  constructor(private titleService: Title, private kioskService: KioskService, private router: Router, private alertService: AlertService) {
     this.titleService.setTitle("Kiosk Homepage - Smart City Herentals");
+    this.loadMessages();
   }
 
   ngOnInit(): void {
+  }
+
+  loadMessages() {
+    this.kioskService.getMessages().subscribe(
+      result => this.messages = result,
+      error => this.alertService.error('Er is iets misgelopen...', 'Berichten konden niet worden geladen. Probeer het later opnieuw.')
+    )
+  }
+
+  goToMessage(messageID: number) {
+    this.router.navigate(['kiosk/stadsnieuws/' + messageID]);
   }
 
   redirectTo(route: string) {
