d23dfd373cd3906c13fb894d8b595d91bff4c059
Seppe Alaerts <r0749143@student.thomasmore.be>
2021-02-11 21:03:53 +0100

    quick survey

--- src/app/kiosk/home/home.component.ts
+++ src/app/kiosk/home/home.component.ts
@@ -1,23 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { Router } from '@angular/router';
 import { AlertService } from 'src/app/shared/alert/services/alert.service';
 import { Message } from 'src/app/shared/models/message.model';
-import { Survey } from 'src/app/shared/models/survey.model';
+import { QuestionComponent } from '../question/question.component';
 import { KioskService } from '../services/kiosk.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class KioskHomeComponent implements OnInit {
 
+  @ViewChild(QuestionComponent) component!: QuestionComponent;
+
   messages: Message[] = [];
+
+  questions: any[] = [];
+  multiItems: any[] = [];
+
+  question: any;
+
+  loadingOQ: boolean = true;
+  loadingMQ: boolean = true;
+  loadingMQI: boolean = true;
+  loadingPost: boolean = false;
 
   constructor(private titleService: Title, private kioskService: KioskService, private router: Router, private alertService: AlertService) {
     this.titleService.setTitle("Kiosk Homepage - Smart City Herentals");
     this.loadMessages();
+    this.loadQuestions();
   }
 
   ngOnInit(): void {
@@ -34,8 +47,50 @@ export class KioskHomeComponent implements OnInit {
     this.router.navigate(['kiosk/stadsnieuws/' + messageID]);
   }
 
+  loadQuestions() {
+    this.kioskService.getMultiQuestionsQuickSurvey().subscribe(
+      result => {
+        result.forEach(question => {
+          this.questions.push(question);
+          this.kioskService.getMultiItems(question.id).subscribe(
+            result2 => {
+              result2.forEach(item => {
+                this.multiItems.push(item);
+              });
+              this.loadingMQI = false;
+            },
+            error => this.alertService.error('Er is iets misgelopen...', 'Bevraging kon niet worden opgehaald. Probeer het later opnieuw.')
+          )
+        });
+        this.loadingMQ = false;
+        this.question = this.questions[Math.floor(Math.random() * Math.floor(this.questions.length))];
+      },
+      error => this.alertService.error('Er is iets misgelopen...', 'Bevraging kon niet worden opgehaald. Probeer het later opnieuw.')
+    );
+    this.kioskService.getOpenQuestionsQuickSurvey().subscribe(
+      result => {
+        result.forEach(question => {
+          this.questions.push(question);
+        });
+        this.loadingOQ = false;
+        this.question = this.questions[Math.floor(Math.random() * Math.floor(this.questions.length))];
+      },
+      error => this.alertService.error('Er is iets misgelopen...', 'Bevraging kon niet worden opgehaald. Probeer het later opnieuw.')
+    );
+  }
+
   redirectTo(route: string) {
     this.router.navigateByUrl(route);
+  }
+
+  saveAnswers() {
+    this.loadingPost = true;
+    this.component.saveAnswer();
+  }
+
+  onStopLoading() {
+    this.loadingPost = false;
+    this.alertService.success('Antwoord opgeslagen', 'Bedankt voor uw bijdrage.')
   }
 
 }
